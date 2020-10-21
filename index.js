e954799c683e7265b9a61f5a2f25c724a1d7c785
Alex Zhang <53130672+zhangerbanger@users.noreply.github.com>
2020-10-21 17:10:00 -0400

    Ready for awesome:

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+
 const express = require('express');
 const path = require('path');
+const generatePassword = require('password-generator');
 
 const app = express();
 
@@ -8,17 +10,17 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 
 // Put all API endpoints under '/api'
 app.get('/api/passwords', (req, res) => {
+  const count = 5;
 
-  const object = {
-  firstName: "John",
-  lastName: "Doe",
-  age: 50,
-  eyeColor: "blue"
-};
+  // Generate some passwords
+  const passwords = Array.from(Array(count).keys()).map(i =>
+    generatePassword(12, false)
+  )
 
   // Return them as json
-  res.json(object);
+  res.json(passwords);
 
+  console.log(`Sent ${count} passwords`);
 });
 
 // The "catchall" handler: for any request that doesn't
@@ -30,4 +32,37 @@ app.get('*', (req, res) => {
 const port = process.env.PORT || 5000;
 app.listen(port);
 
-console.log(`listening on ${port}`);
+console.log(`Password generator listening on ${port}`);
+//const express = require('express');
+//const path = require('path');
+//
+//const app = express();
+//
+//// Serve static files from the React app
+//app.use(express.static(path.join(__dirname, 'client/build')));
+//
+//// Put all API endpoints under '/api'
+//app.get('/api/passwords', (req, res) => {
+//
+//  const object = {
+//  firstName: "John",
+//  lastName: "Doe",
+//  age: 50,
+//  eyeColor: "blue"
+//};
+//
+//  // Return them as json
+//  res.json(object);
+//
+//});
+//
+//// The "catchall" handler: for any request that doesn't
+//// match one above, send back React's index.html file.
+//app.get('*', (req, res) => {
+//  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+//});
+//
+//const port = process.env.PORT || 5000;
+//app.listen(port);
+//
+//console.log(`listening on ${port}`);
